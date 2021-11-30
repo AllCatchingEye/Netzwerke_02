6fe526ba733239db013879e46c098b95c40d91b7
Nicolas Alexander Lerch <nicolas.lerch@hm.edu>
2021-11-30 13:39:12 +0100

    Georg und Nicolas Server und Sender

--- src/abgabe6/Client.java
+++
@@ -1,9 +0,0 @@
-package abgabe6;
-
-/**
- * Klasse des Clients zum versandt von Packeten an den Messungs-Server.
- * @author Georg Lang, Nicolas Lerch.
- * @version 26.11.2021.
- */
-public class Client {
-}
--- src/abgabe6/Server.java
+++
@@ -1,9 +0,0 @@
-package abgabe6;
-
-/**
- * Klasse des Messungs-Servers.
- * @author Georg Lang, Nicolas Lerch.
- * @version 26.11.2021.
- */
-public class Server {
-}
