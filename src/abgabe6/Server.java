03097440ba1ec643b4f77f8f1b4594fffe5090fb
Nicolas Alexander Lerch <nicolas.lerch@hm.edu>
2021-12-02 12:14:13 +0100

    Georg Nicolas, versuch exception im tcpServer zu fixen fragen noch offen

--- src/abgabe6/Server.java
+++ src/abgabe6/Server.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
